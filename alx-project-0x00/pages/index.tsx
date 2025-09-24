e162d68649fee62ebd50f039c086f80a895d6df7
dev-dorn <berrymundia@gmail.com>
2025-09-24 16:40:38 +0300

    yy

--- alx-project-0x00/pages/index.tsx
+++ alx-project-0x00/pages/index.tsx
@@ -1,6 +1,6 @@
 
 
-const Home : React.FC= () => {
+const Home: React.FC= () => {
   return (
     <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
       <h1>Airbnb Application Clone system</h1>
